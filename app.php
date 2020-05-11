cd9e1912ccbd21ef9a4aba3dc541440bb82fc012
Denuis Kireev <two.fl3xice@yandex.ru>
2020-05-11 18:42:33 +0400

    Создан главный файл откуда всё начинается

---
+++ app.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/*
+ *
+ * Иницилизируем приложение
+ *
+ * Вызываем авто-загрузчик классов от composer.
+ *
+ */
+
+require_once __DIR__.
+    DIRECTORY_SEPARATOR.
+    "vendor".
+    DIRECTORY_SEPARATOR.
+    "autoload.php";
+
