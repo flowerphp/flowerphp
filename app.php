63fe2701256a8bcfd1f311504a30f1f99f631665
Denuis Kireev <two.fl3xice@yandex.ru>
2020-05-19 22:43:53 +0400

    Добавленны комментарии в app.php и добавлен пункт в файле config.json по включению или отключению требования подключения к бд

--- app.php
+++ app.php
@@ -2,41 +2,33 @@
 
 use flowerphp\App;
 use flowerphp\Assets;
-use flowerphp\ClassesObserve\Observer;
 use Klein\Klein;
 use League\Flysystem\Adapter\Local;
 
-/*
- *
- * Иницилизируем приложение
- *
- * Вызываем авто-загрузчик классов от composer.
- *
- */
-
-
+// Вызываем авто-загрузчик классов Composer
 require_once __DIR__."/vendor/autoload.php";
 
+// Создание адаптера для абстрактной файловой системы
 $Adapter = new Local(__DIR__);
 
+// Создание экземпляра приложения класса App
 $App = new App($Adapter);
 
+// Создание экземпляра роутера от https://github.com/klein/klein.php
 $Router = new Klein();
 
-$ObserveAppClass = new Observer("App");
-
-$ObserveAppClass->observe();
-
-print $ObserveAppClass->nameClass;
-
+// Вызов всех пользовательских роутов
 require_once __DIR__."/app/routes/web.php";
 
+// Создание анонимного класса Assets функцию construct который будет вызван
 (new Assets($App,$Router));
 
+// Обработчик ошибок
 $Router->onHttpError(function ($code, $router) use ($App) {
     $router->response()->body(
         $App->getView()->get("errors.$code")
     );
 });
 
+// Запустить работу роутера
 $Router->dispatch();
